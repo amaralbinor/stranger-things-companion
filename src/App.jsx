daafc162f879eae6730cf8e68c8c9b7e102766ad
Luis Fernando Balcero Gomez <ferbalcerog@192.168.81.12>
2026-05-18 15:50:25 -0500

    Agregado login y mejora visual de series

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,27 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Characters from "./pages/Characters";
 import Episodes from "./pages/Episodes";
+import Products from "./pages/Products";
 import Login from "./pages/Login";
 import Series from "./pages/Series";
 
 function App() {
+  return (
+    <>
+      <Navbar />
 
-return (
-
-<>
-
-<Navbar />
-
-<Routes>
-
-<Route path="/" element={<Landing />} />
-<Route path="/home" element={<Home />} />
-<Route path="/about" element={<About />} />
-<Route path="/characters" element={<Characters />} />
-<Route path="/episodes" element={<Episodes />} />
-<Route path="/login" element={<Login />} />
-<Route path="/series" element={<Series />} />
-
-</Routes>
-
-</>
-
-);
-
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/series" element={<Series />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
